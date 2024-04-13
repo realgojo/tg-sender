c3b1868d0af3ab5fc8dca2355871a459467feed4
realgojo <166885363+realgojo@users.noreply.github.com>
2024-04-13 05:29:04 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Download gojo.py from GitHub
+https://raw.githubusercontent.com/realgojo/tg-sender/main/gsender.py
+# Install Python3 and pip
+sudo apt-get update
+sudo apt-get install -y python3 python3-pip
+
+# Install required Python packages
+sudo pip3 install -U python-telegram-bot
+
+# Create a symbolic link to make the script accessible globally
+sudo ln -s $(pwd)/gsender.py /usr/local/bin/gsender
+
+echo "Installation complete. You can now use 'gsender' command to run the script."
