7df775f485850db14e70b25ab02335eec25c8e24
realgojo <166885363+realgojo@users.noreply.github.com>
2024-04-13 05:43:54 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,4 +16,14 @@ chmod +x gsender.py
 # Move gsender.py to /usr/local/bin and rename it to gsender
 sudo mv gsender.py /usr/local/bin/gsender
 
+# Create a wrapper script for gsender.py
+echo -e '#!/usr/bin/env python3\n' > gsender
+echo -e 'from gsender import main\n' >> gsender
+echo -e 'if __name__ == "__main__":\n' >> gsender
+echo -e '    import asyncio\n' >> gsender
+echo -e '    asyncio.run(main())' >> gsender
+
+# Set executable permission for the wrapper script
+sudo chmod +x gsender
+
 echo "Installation complete. You can now use 'gsender' command to run the script."
